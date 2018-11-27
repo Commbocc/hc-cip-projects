f3db4ca678170208e2bc48d66a8925ace0a43aa8
Kevin McCarthy <mccarthyk@hcflgov.net>
2018-11-27 12:58:08 -0500

    remove 2 column project layout, add desc1 to overview

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -28,7 +28,7 @@
         <div class="quick-fact">
           <h3>Overview</h3>
           <ul>
-            <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
+            <li>{{ project.attributes.DESC1 }}</li>
             <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
             <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
             <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
--- src/components/ProjectIndex.vue
+++ src/components/ProjectIndex.vue
@@ -2,47 +2,46 @@
   <div class="py-5 bg-white">
     <div class="container">
 
-      <div class="card">
-        <table class="table table-striped tabel-bordered table-responsive mb-0">
-          <thead>
-            <tr>
-              <th>Project</th>
-              <th>Type</th>
-              <th>Phase</th>
-              <th>District</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="project in projects">
-              <td>
-                <router-link :to="project.path">
-                  {{ project.attributes.name }}
-                </router-link>
-                <br>
-                <small class="text-muted">
-                  #{{ project.attributes.PLACENUMSL }}
-                </small>
-              </td>
-              <td class="small text-muted">
-                {{ project.attributes.Major_Category }} -
-                {{ project.attributes.SHORT_DESC }}
-              </td>
-              <td class="small text-muted">
-                {{ project.attributes.Current_Phase }}
-              </td>
-              <td class="small text-muted">
-                {{ project.attributes.Commisioner_District }} - {{ project.attributes.DESC5 }}
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      <table class="table table-striped tabel-bordered table-responsive mb-0">
+        <thead>
+          <tr>
+            <th>Project</th>
+            <th>Type</th>
+            <th>Phase</th>
+            <th>District</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="project in filteredProjects">
+            <td>
+              <router-link :to="project.path">
+                {{ project.attributes.name }}
+              </router-link>
+              <br>
+              <small class="text-muted">
+                #{{ project.attributes.PLACENUMSL }}
+              </small>
+            </td>
+            <td class="small text-muted">
+              {{ project.attributes.Major_Category }} -
+              {{ project.attributes.SHORT_DESC }}
+            </td>
+            <td class="small text-muted">
+              {{ project.attributes.Current_Phase }}
+            </td>
+            <td class="small text-muted">
+              {{ project.attributes.Commisioner_District }} - {{ project.attributes.DESC5 }}
+            </td>
+          </tr>
+        </tbody>
+      </table>
 
     </div>
   </div>
 </template>
 
 <script>
+import _ from 'lodash'
 import Project from '../store/project'
 
 export default {
@@ -58,6 +57,14 @@ export default {
     }).catch(err => console.error(err)).finally(() => {
       this.$store.state.loading = false
     })
+  },
+  computed: {
+    filteredProjects () {
+      // filter
+      // sort
+      // paginate
+      return this.projects
+    }
   }
 }
 </script>
