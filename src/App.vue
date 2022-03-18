93a7d0891670aa37142e9100ac23178036a3c299
Jordan Pisani <jordanpisani@pm.me>
2022-03-18 08:37:03 -0400

    init vite

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,21 @@
-<template>
-  <div class="">
-
-    <nav aria-label="breadcrumb" class="bg-white">
-      <ol class="breadcrumb bg-white">
-        <li v-if="!isIndex" class="breadcrumb-item">
-          <router-link to="/" class="">
-            Capital Improvement Projects
-          </router-link>
-        </li>
-        <li class="breadcrumb-item active" aria-current="page">{{ $store.state.pageTitle }}</li>
-      </ol>
-    </nav>
-
-    <div v-if="$store.state.loading" class="py-5">
-      <div class="container">
-        <div class="h1 text-center text-secondary py-5">
-          Loading <i class="fas fa-spinner fa-pulse"></i>
-        </div>
-      </div>
-    </div>
-
-    <router-view :key="$route.fullPath" v-show="!$store.state.loading"></router-view>
-
-  </div>
-</template>
-
-<script>
-import router from './router'
-import store from './store'
-
-export default {
-  router,
-  store,
-  computed: {
-    isIndex () {
-      return (this.$route.name == "Index")
-    }
-  }
-}
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<style src="./assets/main.css"></style>
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
