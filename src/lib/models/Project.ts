f93a161f2db0552627b66c083fa433ce18d65e00
Jordan Pisani <jordanpisani@pm.me>
2022-03-23 14:02:45 -0400

    complete show page

--- src/lib/models/Project.ts
+++ src/lib/models/Project.ts
@@ -7,6 +7,23 @@ export default class Project {
   phase: string
   community: string
   district: string
+  planningDate: string
+  phaseGraphic: string
+  factSheet: string
+  designDate: string
+  constructionStart: string
+  constructionEnd: string
+  descriptionOne: string
+  descriptionTwo: string
+  objectiveOne: string
+  objectiveTwo: string
+  objectiveThree: string
+  objectiveFour: string
+  lastEditedDate: string
+  projectBudget: string
+  planningBudget: string
+  designBudget: string
+  constructionBudget: string
 
   geometry?: __esri.Geometry
 
@@ -19,7 +36,23 @@ export default class Project {
     this.phase = attributes['Current_Phase']
     this.community = attributes['Community']
     this.district = attributes['Commisioner_District']
-    // this.someMoneyValue = this.currency(attributes['A_Number_Field'])
+    this.planningDate = attributes['PlanningDate']
+    this.factSheet = attributes['thumb_url']
+    this.designDate = attributes['DesignDate']
+    this.constructionStart = attributes['Constr_Start']
+    this.constructionEnd = attributes['Constr_End']
+    this.descriptionOne = attributes['DESC1']
+    this.descriptionTwo = attributes['DESC2']
+    this.objectiveOne = attributes['Obj1']
+    this.objectiveTwo = attributes['Obj2']
+    this.objectiveThree = attributes['Obj3']
+    this.objectiveFour = attributes['Obj4']
+    this.lastEditedDate = attributes['last_edited_date']
+    this.phaseGraphic = attributes['PhaseGraphic']
+    this.projectBudget = this.currency(attributes['Project_Budget'])
+    this.planningBudget = this.currency(attributes['PlanningBudget'])
+    this.designBudget = this.currency(attributes['DesignBudget'])
+    this.constructionBudget = this.currency(attributes['Constr_Budget'])
 
     this.geometry = geometry?.toJSON()
   }
